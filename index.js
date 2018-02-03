99d6f71f33fad0b7f451cdb4e5f9ccd24f5bd030
ceptes <you@example.comprateek@ceptes.com>
2018-02-03 19:30:50 +0530

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 var { ObjectID } = require("mongodb");
 
+const _ = require("lodash");
+
 const keys = require('./server/config/keys');
 const { Todo } = require('./server/models/todo');
 mongoose.connect(keys.mongoURL);
@@ -54,8 +56,36 @@ app.post('/todos',  async (req, res) => {
     }
 });
 
+
+app.patch('/todos/:id', async  (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+
+    }
+    try{
+        var updatedTodo = await Todo.findByIdAndUpdate(id, {$set:body},{new :true});
+        if(!updatedTodo){
+            return res.status(404).send();
+        }
+        res.send(updatedTodo);
+    }catch(e){
+        res.status(400).send();
+    }
+   
+
+});
+
 app.delete('/todos/:id', async (req, res) => {
     var id = req.params.id;
+
     if(!ObjectID.isValid(id)){
         res.status(404).send();
     }
@@ -72,6 +102,8 @@ app.delete('/todos/:id', async (req, res) => {
 
 
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
